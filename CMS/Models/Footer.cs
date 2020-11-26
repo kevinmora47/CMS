5178a7140e2a3da9e3d72830b96037707708973f
kevinmora47 <kevinmora47@gmail.com>
2020-11-25 20:16:32 -0600

    Footer creation

---
+++ CMS/Models/Footer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CMS.Models
+{
+    public partial class Footer
+    {
+        public int IdFooter { get; set; }
+        public string CategoryOne { get; set; }
+        public string CategoryTwo { get; set; }
+        public string CategoryThree { get; set; }
+    }
+}
