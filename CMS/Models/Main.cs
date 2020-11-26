2cfcef307c1003648561499c3e62b4a4421e1cf2
kevinmora47 <kevinmora47@gmail.com>
2020-11-25 20:16:50 -0600

    main creation

---
+++ CMS/Models/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CMS.Models
+{
+    public partial class Main
+    {
+        public int IdMain { get; set; }
+        public string Header { get; set; }
+        public string Legend { get; set; }
+        public string TableTitle { get; set; }
+        public string CategoryOne { get; set; }
+        public string CategoryTwo { get; set; }
+        public string CategoryThree { get; set; }
+        public string CategoryFour { get; set; }
+        public string CategoryOneValue { get; set; }
+        public string CategoryTwoValue { get; set; }
+        public string CategoryThreeValue { get; set; }
+        public string CategoryFourValue { get; set; }
+        public DateTime? Updated { get; set; }
+    }
+}
