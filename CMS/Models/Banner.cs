9dab68891298a4b0abc954bdbc0562e655fc5c10
kevinmora47 <kevinmora47@gmail.com>
2020-11-25 20:16:15 -0600

    Header creation

---
+++ CMS/Models/Banner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CMS.Models
+{
+    public partial class Banner
+    {
+        public int IdBanner { get; set; }
+        public string Header { get; set; }
+    }
+}
