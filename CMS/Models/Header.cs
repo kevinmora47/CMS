e3e5e40c5dee1f57cde60914afc6460358af1ce6
kevinmora47 <kevinmora47@gmail.com>
2020-11-25 20:16:43 -0600

    Header creation

---
+++ CMS/Models/Header.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CMS.Models
+{
+    public partial class Header
+    {
+        public int IdHeader { get; set; }
+        public string CompanyName { get; set; }
+        public string LegendName { get; set; }
+        public string SubLegendName { get; set; }
+        public string Plans { get; set; }
+    }
+}
