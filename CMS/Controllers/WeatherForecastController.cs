3ead8614b06add3ff173799ae3f5ad196622946b
kevinmora47 <kevinmora47@gmail.com>
2020-11-27 15:29:48 -0600

    Edit of foreweathercast controller.

--- CMS/Controllers/WeatherForecastController.cs
+++ CMS/Controllers/WeatherForecastController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CMS.DataLayer.Header;
+using CMS.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -23,17 +25,6 @@ namespace CMS.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
+
     }
 }
