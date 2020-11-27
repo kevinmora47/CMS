a9be68a2e1b49e6c39a6aec481a9c4931f164753
kevinmora47 <kevinmora47@gmail.com>
2020-11-27 15:30:04 -0600

    Edit method of banner.

---
+++ CMS/DataLayer/Banner/BannerEdit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.DataLayer.Banner
+{
+    public class BannerEdit
+    {
+        public bool Edit(Models.Banner banner)
+        {
+            try
+            {
+                using (Models.CMSContext db = new Models.CMSContext())
+                {
+                    var id = db.Banners.Find(banner.IdBanner);
+                    id.Header = banner.Header;
+                    db.Entry(id).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+
+            }
+        }
+    }
+}
