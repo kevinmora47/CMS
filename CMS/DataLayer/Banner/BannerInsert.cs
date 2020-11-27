2e9cc25bdc3445e566aab92826a06198080f93e8
kevinmora47 <kevinmora47@gmail.com>
2020-11-27 15:30:43 -0600

    Insert method banner

---
+++ CMS/DataLayer/Banner/BannerInsert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.DataLayer.Banner
+{
+    public class BannerInsert
+    {
+        public bool Insert(Models.Banner banner)
+        {
+            using (Models.CMSContext db = new Models.CMSContext())
+            {
+                db.Banners.Add(banner);
+            }
+            return true;
+        }
+    }
+}
