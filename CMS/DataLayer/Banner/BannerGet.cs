e561f427e10d85ac769a978edb3194c1b59e4673
kevinmora47 <kevinmora47@gmail.com>
2020-11-27 15:30:25 -0600

    Banner get method.

--- CMS/DataLayer/Banner/BannerGet.cs
+++ CMS/DataLayer/Banner/BannerGet.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace CMS.DataLayer.Header
 {
     public class BannerGet
     {
-       
-        public List<Banner> Get() {
+
+        public List<Models.Banner> Get()
+        {
             using (CMS.Models.CMSContext db = new CMS.Models.CMSContext())
             {
                 var List = (from d in db.Banners
