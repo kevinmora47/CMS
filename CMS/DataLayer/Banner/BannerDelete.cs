d565a8f95a755e5369284df47c23c5d24df44024
kevinmora47 <kevinmora47@gmail.com>
2020-11-29 21:35:28 -0600

    Delete method banner

---
+++ CMS/DataLayer/Banner/BannerDelete.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS.DataLayer.Banner
+{
+    public class BannerDelete
+    {
+        public bool Delete(Models.Banner banner)
+        {
+            try
+            {
+                using (Models.CMSContext db = new Models.CMSContext())
+                {
+                    var id = db.Banners.Find(banner.IdBanner);
+                    db.Banners.Remove(id);
+                    db.SaveChanges();
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;
+
+            }
+        }
+    }
+}
